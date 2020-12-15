c0e3e15d797fdac46fc3def029bd0583f08b0240
suetarazi <suemachtley@gmail.com>
2020-12-14 21:10:48 -0800

    done with for loops and while loops

--- lessons9-11.go
+++ lessons9-11.go
@@ -20,4 +20,25 @@ func main() {
 		fmt.Printf("wait %d years", 18-age)
 	}
 	fmt.Println("after if")
+
+	for_loop_func()
+}
+
+func for_loop_func() {
+	x := 3
+	for x<5 {
+		fmt.Println(x)
+		x++
+	}
+
+	for x:=0; x<=5; x++ {
+		fmt.Println(x)
+	}
+
+	for x:=0; x<=1000; x++ {
+		if x != 0 && x % 3 == 0 ** x % 7 == 0 {
+			fmt.Println(x)
+			continue
+		}
+	}
 }
